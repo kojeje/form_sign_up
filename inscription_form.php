4f3527932f17762ff88480dbfc9c86fe9ab60bd5
kojeje <kojeje.dev@gmail.com>
2018-11-01 20:24:55 +0100

    local

--- inscription_form.php
+++ inscription_form.php
@@ -24,37 +24,216 @@
 <!--    Titre-->
         <form action="crea_compte.php" method="post" enctype="multipart/form-data" class="form-horizontal">
 
-<!--   *******************************  -->
+<!--   ********* Identité ************  -->
 
-<!--             TITRE -->
-                <div class="row">
-                        <div class="form-group">
-                                <label for="titre" class="control-label col-sm-4"><h3>TITRE</h3></label>
-                                <input type="text" name="titre"  class="input col-sm-8">
+        <!--             NOM -->
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="last_name" class="control-label col-sm-2"><h3>Nom</h3></label>
+                                        <input type="text" name="last_name"  class="input col-sm-8">
+                                </div>
+                        </div>
+
+        <!--   *******************************  -->
+
+        <!--            PRENOM -->
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="first_name" class="control-label col-sm-2"><h4>Prénom</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="first_name"></input>
+                                </div>
+                        </div>
+        <!--   *******************************  -->
+
+        <!--            PSEUDO -->
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="pseudo" class="control-label col-sm-2"><h4>Pseudo</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="pseudo"></input>
+                                </div>
+                        </div>
+<!--   ***** coordonnées numériques *****************  -->
+
+                <div id="coo">
+
+        <!--            EMAIL -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="email" class="control-label col-sm-2"><h4>Email</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="email" class="input col-sm-8" name="email"></input>
+                                </div>
+                        </div>
+        <!--   *******************************  -->
+
+
+        <!--            Tel -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="tel" class="control-label col-sm-2"><h4>Tel</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="tel" class="input col-sm-8" name="tel"></input>
+                                </div>
                         </div>
                 </div>
+<!--   ************** Adresse postale *****************  -->
+
+                <div id="address">
+
+        <!--            address1 -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="address1" class="control-label col-sm-2"><h4>address 1</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="address1"></input>
+                                </div
+                        </div>
+<!--   *******************************  -->
+
+        <!--            address2 -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="address2" class="control-label col-sm-2"><h4>address 2</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="address2"></input>
+                                </div>
+                        </div>
 
 <!--   *******************************  -->
 
-<!--            DESCRIPTION -->
-                <div class="row">
-                        <div class="description">
-                                <label for="description" class="control-label col-sm-4"><h4>Description</h4></label>
-                                <div class="col-sm-offset-2"></div>
-                                <textarea rows="4" cols="8" class="description col-sm-8" name="description"></textarea>
+        <!--             zip code-->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="zip" class="control-label col-sm-2"><h4>Zip Code</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="zip"></input>
+                                </div>
+                        </div>
+<!--   *******************************  -->
+
+        <!--             Town-->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="town" class="control-label col-sm-2"><h4>Town</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="town"></input>
+                                </div>
+                        </div>
+
+<!--   *******************************  -->
+
+        <!--           Country-->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="country" class="control-label col-sm-2"><h4>Country</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="country"></input>
+                                </div>
                         </div>
                 </div>
+<!--   *************** données personnelles ****************  -->
+
+                <div id="perso">
+
+        <!--            Birthdate-->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="birthdate" class="control-label col-sm-2"><h4>Your Birthdate</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="date" class="input col-sm-8" name="birthdate"></input>
+                                </div>
+                        </div>
+<!--   *******************************  -->
+
+        <!--            job -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="job" class="control-label col-sm-2"><h4>Your Job</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="job"></input>
+                                </div>
+                        </div>
+<!--   *******************************  -->
+
+        <!--            hobby1 -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="hobby1" class="control-label col-sm-2"><h4>hobby 1</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="hobby1"></input>
+                                </div>
+                        </div>
 
 <!--   *******************************  -->
 
-<!--             FILE -->
+        <!--            hobby2 -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="hobby2" class="control-label col-sm-2"><h4>hobby 2</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="hobby2"></input>
+                                </div>
+                        </div>
+<!--   *******************************  -->
+
+        <!--            hobby3 -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="hobby3" class="control-label col-sm-2"><h4>hobby 3</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="hobby3"></input>
+                                </div>
+                        </div>
+<!--   *******************************  -->
+
+        <!--            hobby4 -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="hobby4" class="control-label col-sm-2"><h4>hobby 4</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <input type="text" class="input col-sm-8" name="hobby4"></input>
+                                </div>
+                        </div>
+<!--   *******************************  -->
+
+        <!--            about me  -->
+
+                        <div class="row">
+                                <div class="form-group">
+                                        <label for="about_me" class="control-label col-sm-2"><h4>About me</h4></label>
+                                        <div class="offset-sm-2"></div>
+                                        <textarea  class="input col-sm-8" name="about_me"></textarea>
+                                </div>
+                        </div>
+
+                </div>
+<!--   *******************************  -->
+
+
+        <!--             FILE -->
+
                 <div class="row">
                         <div id="file">
-                                <div class="form-group file col-sm-12">
-                                        <label id="lbl_fichier" for="fichier" class="control-label "><h4 id="file_title">Fichier</h4></label>
-
+                                <div class="form-group file ">
+                                        <label id="lbl_fichier" for="fichier" class="control-label col-sm-3"><h4 id="file_title">Fichier</h4></label>
+                                        <div class="offset-sm-4"></div>
                                         <div >
-                                                <input id="fichier" name="fichier" type="file"/>
+                                                <input id="fichier" class="col-sm-4" name="fichier" type="file"/>
                                         </div>
                                 </div>
                         </div>
@@ -63,34 +242,15 @@
 
 <!--   *******************************  -->
 
-<!--             GALLERIE-->
-
-                <div class="form-group">
-
-                        <label for="gallerie" class="control-label col-sm-6">
-                                <h4>Gallerie</h4>
-                        </label>
-
-                        <input type="text" class="input col-sm-6" name="gallerie">
-
-                </div>
-
-<!--   *******************************  -->
-
-<!--              DATE-->
-                <div class="form-group">
-                        <label for="date" class="control-label col-sm-6"><h4>Date de création</h4></label>
-                        <input type="date" class="input col-sm-6" name="date">
-                </div>
-
-
 <!--   *******************************  -->
 
 <!--              Submit-->
                 <div class="form-group">
-                        <div class="col-sm-3 ">
+                        <div class="offset-sm-4"></div>
+                        <div class="col-sm-4 ">
                                 <input id="uploader" name="uploader" type="submit" value="envoyer" class="btn btn-primary"/>
                         </div>
+                        <div class="offset-sm-4"></div>
                 </div>
 
         </form>
